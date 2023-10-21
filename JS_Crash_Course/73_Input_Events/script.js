4cf10aed8520616ba0858a8e465d0b968b07f65e
ZainabTechWizard <noorzainabnaeem@gmail.com>
2023-10-21 17:25:32 +0500

    73_Input_Events

---
+++ JS_Crash_Course/73_Input_Events/script.js
@@ -0,0 +1,30 @@
+const itemInput = document.getElementById('item-input');
+const priorityInput = document.getElementById('priority-input');
+const checkbox = document.getElementById('checkbox');
+const heading = document.getElementById('h1');
+
+function onInput (e) {
+    heading.textContent = e.targetvalue;
+}
+
+function onChecked(e) {
+    const isChecked = e.target.checked;
+    heading.textContent = isChecked ? 'Checked' : 'Not Checked';
+}
+
+function onFocus() {
+    console.log('Input is focused');
+    itemInput.style.outlineStyle = 'solid';
+    itemInput.style.outlineWidth = '1px';
+    itemInput.style.outlineColor = 'red';
+}
+
+function onBlur() {
+    console.log('Input is not focused');
+    itemInput.style.outlineStyle = 'none';
+}
+
+itemInput.addEventListener('input', onInput);
+priorityInput.addEventListener('change', onInput);
+checkbox.addEventListener('focus', onFocus);
+itemInput.addEventListener('blur', onBlur);
