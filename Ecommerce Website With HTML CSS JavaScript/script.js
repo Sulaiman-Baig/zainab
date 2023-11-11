578b50dfa5e6fb767f0cbf0b825cb2a4493843ed
ZainabTechWizard <noorzainabnaeem@gmail.com>
2023-11-11 23:35:59 +0500

    Ecommerce_Website

--- Ecommerce Website With HTML CSS JavaScript/script.js
+++ Ecommerce Website With HTML CSS JavaScript/script.js
@@ -15,3 +15,19 @@
 //     diffY = Math.abs(x - e.offsetY); if (diffX >= move);
 // })
 // Script for navigation bar
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active')
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active')
+    })
+}
